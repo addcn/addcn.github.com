fce997a0e4328c8949f28d2a489b485876671b56
addcns <huibo@addcn.com>
2013-04-08 17:33:30 +0800

    Site updated at 2013-04-08 09:33:30 UTC

---
+++ javascripts/custom-footer.js
@@ -0,0 +1,14 @@
+(function($) {
+	jQuery(document).ready(function() {
+		// µ¼º½µ±Ç°Ò³
+		var pathname = window.location.pathname;
+		pathname = pathname.toLowerCase();
+		if ( (pathname.indexOf("archives") == -1) && (pathname.indexOf("categories") == -1) ){
+			jQuery("#header nav li").attr("class", "menu-item");
+			jQuery("#header nav li#nav-index").attr("class", "menu-item current-menu-item");
+		}else{
+			jQuery("#header nav li").attr("class", "menu-item");
+			jQuery("#header nav li#nav-archives").attr("class", "menu-item current-menu-item");
+		}
+	});
+})(jQuery);
