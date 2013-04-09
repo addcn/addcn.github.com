59635989e70e2a4075248d77e4fae5f3d8437b0b
addcns <huibo@addcn.com>
2013-04-09 15:15:24 +0800

    Site updated at 2013-04-09 07:15:24 UTC

--- javascripts/custom-footer.js
+++ javascripts/custom-footer.js
@@ -4,13 +4,21 @@
 		var pathname = window.location.pathname;
 		pathname = pathname.toLowerCase();
 		if ( (pathname.indexOf("archives") == -1) && (pathname.indexOf("categories") == -1) ){
-			jQuery("#header nav li").attr("class", "menu-item");
-			jQuery("#header nav li#nav-index").attr("class", "menu-item current-menu-item");
+			jQuery("#container-header nav li").attr("class", "menu-item");
+			jQuery("#container-header nav li#nav-index").attr("class", "menu-item current-menu-item");
 		}else{
-			jQuery("#header nav li").attr("class", "menu-item");
-			jQuery("#header nav li#nav-archives").attr("class", "menu-item current-menu-item");
+			jQuery("#container-header nav li").attr("class", "menu-item");
+			jQuery("#container-header nav li#nav-archives").attr("class", "menu-item current-menu-item");
 		}
-		//
+		// ·µ»Ø¶¥²¿
 		jQuery.scrolltop.scrollStart();
+		//
+		jQuery('a[href^="http"],a[href^="https"],a[href^="ftp"]').each(function(){
+			var link = jQuery(this).attr("href");
+			link = link.toLowerCase();
+			if( ( (link.indexOf("http://") != -1) || (link.indexOf("https://") != -1) || (link.indexOf("ftp://") != -1) ) && (link.indexOf("uedao.com") == -1) ){
+				jQuery(this).attr('target', '_blank');
+			}
+		});
 	});
 })(jQuery);
